ad470cf4e2380630a458a3fe0d442aec883a6edc
George Bryant <george.s.bryant@gmail.com>
2014-10-31 12:08:47 +0000

    Added favicon

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,4 @@
 <?php include "./includes/header.php"; ?>
 <h1>Welcome to Gravity Gym</h1>
-<img src="<?php echo $rootpath;?>images/favicon.png">
+<img src="<?php echo $rootpath;?>images/favicon.jpg">
 <?php include "./includes/footer.php"; ?>

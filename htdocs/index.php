09cd03dea3a0d7b4d017cfc2031d9eca7718043c
George Bryant <george.s.bryant@gmail.com>
2014-11-04 15:32:46 +0000

    More testing of git because I'm dumb

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,5 @@
 <?php include "./includes/header.php"; ?>
 <h1>Welcome to Gravity Gym</h1>
+<p>Testing local server</p>
 <img src="<?php echo $rootpath;?>images/favicon.jpg">
 <?php include "./includes/footer.php"; ?>

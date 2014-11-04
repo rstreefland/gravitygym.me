3fb45debfe73905c324cd672d7865577c482a7e6
George Bryant <george.s.bryant@gmail.com>
2014-11-04 15:36:47 +0000

    Revert "More testing of git because I'm dumb"
    This reverts commit 09cd03dea3a0d7b4d017cfc2031d9eca7718043c.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,4 @@
 <?php include "./includes/header.php"; ?>
 <h1>Welcome to Gravity Gym</h1>
-<p>Testing local server</p>
 <img src="<?php echo $rootpath;?>images/favicon.jpg">
 <?php include "./includes/footer.php"; ?>

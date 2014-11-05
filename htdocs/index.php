2c9a77c5b6cbdf14cd1a2475cde6a4ae7cfd83e4
George Bryant <george.s.bryant@gmail.com>
2014-11-05 18:01:40 +0000

    Test from George's laptop, delete me
    Signed-off-by: George Bryant <george.s.bryant@gmail.com>

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,5 @@
 <?php include "./includes/header.php"; ?>
 <h1>Welcome to Gravity Gym</h1>
+<h2>Do you even lift?</h2>
 <img src="<?php echo $rootpath;?>images/favicon.jpg">
 <?php include "./includes/footer.php"; ?>

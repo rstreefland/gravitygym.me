336d38d5a51e12bb09307aa8b9252fd1f029d8c4
Dan Hughes <danhughes.dh+github@gmail.com>
2014-11-13 12:39:05 +0000

    Revert "Add lovely picture to homepage"
    This reverts commit 9cf6175e44472e0ad770f45d748c6c565fc95afe.

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,5 +2,4 @@
 <h1>Welcome to Gravity Gym</h1>
 <h2>Do you even lift?</h2>
 <img src="<?php echo $rootpath;?>images/favicon.jpg">
-<img src="<?php echo $rootpath;?>images/945102_10201014904249081_1604217792_n.jpg">
 <?php include "./includes/footer.php"; ?>

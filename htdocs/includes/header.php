bd7bb48333b8a0c7a77ee3abfff3da3525dd5380
Dan Hughes <danhughes.dh+github@gmail.com>
2014-11-20 18:10:50 +0000

    Give navigation proper tag, tidy comments

--- htdocs/includes/header.php
+++ htdocs/includes/header.php
@@ -85,21 +85,20 @@ function thumb($src, $width) {
 	<body>
 		<!--[if IE]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
 		<header class='block'>
-			<div class="wrap">
+			<nav class="wrap">
 				<div class="float-container">
 					<a class="float-left" href="<?php echo $rootpath;?>">
 						<img src="<?php echo thumb($rootpath.'images/logo_wide_pink.png', 600);?>">
 					</a>
-					<nav class="float-right align-center">
+					<div class="navigation float-right align-center">
 						<a href = "#">About</a
 						><a href = "#">Gallery</a
 						><a href = "#">News</a
 						><a href = "#">Events</a
 						><a href = "#">Community</a
 						><a href = "#">Contact</a>
-					</nav>
 					</div>
 				</div>
-			</div>
+			</nav>
 		</header>
 	<div id="content">

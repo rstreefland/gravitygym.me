1b5826e7893a5e67855bd31b8d89410248910de3
Dan Hughes <danhughes.dh+github@gmail.com>
2014-11-20 18:35:52 +0000

    Add small wrap and current page highlighting

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,6 +3,10 @@
 	<h1>Welcome to Gravity Gym</h1>
 	<h2>Do you even lift?</h2>
 	<p>Lorem ipsum dolor sit amet, <a href="#">consectetur adipiscing elit</a>. Donec blandit non leo eget vestibulum. Proin lacinia massa id mollis blandit. Donec commodo odio vel lectus mollis aliquam. Praesent <a href="#">feugiat urna ut tempor</a> imperdiet. Quisque tincidunt maximus elit a laoreet. Aliquam congue purus sit amet augue imperdiet, ut ullamcorper velit condimentum. Vestibulum laoreet viverra quam et pulvinar. Curabitur consequat luctus justo, suscipit fermentum orci volutpat a. Quisque sollicitudin viverra pellentesque.</p>
-	<p>Vestibulum tortor nisi, posuere eget risus vel, dictum rutrum ex. Praesent sit amet posuere neque, sed convallis arcu. Vestibulum dapibus mollis est, at placerat leo luctus sit amet. Duis justo odio, tincidunt ac pulvinar nec, dapibus non neque. Quisque eget risus et urna tempor ultrices. Etiam id vehicula massa, eget mattis lectus. Proin consequat, elit id consequat aliquet, odio justo posuere nisl, non tempus leo purus quis erat. Fusce nibh lorem, fermentum sit amet tortor at, molestie molestie neque. Vestibulum bibendum dolor id lacus euismod scelerisque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel dolor sit amet eros egestas imperdiet vitae sed sapien. Aenean facilisis orci purus.</p>
+</div>
+<div class="block has-bottom-image">
+	<div class="wrap small">
+		<p>Vestibulum tortor nisi, posuere eget risus vel, dictum rutrum ex. Praesent sit amet posuere neque, sed convallis arcu. Vestibulum dapibus mollis est, at placerat leo luctus sit amet. Duis justo odio, tincidunt ac pulvinar nec, dapibus non neque. Quisque eget risus et urna tempor ultrices. Etiam id vehicula massa, eget mattis lectus. Proin consequat, elit id consequat aliquet, odio justo posuere nisl, non tempus leo purus quis erat. Fusce nibh lorem, fermentum sit amet tortor at, molestie molestie neque. Vestibulum bibendum dolor id lacus euismod scelerisque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel dolor sit amet eros egestas imperdiet vitae sed sapien. Aenean facilisis orci purus.</p>
+	</div>
 </div>
 <?php include "./includes/footer.php"; ?>

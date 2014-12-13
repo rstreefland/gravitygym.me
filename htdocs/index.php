89500b3f6a4485784689e2199fefbca016d45dfb
Dan Hughes <danhughes.dh+github@gmail.com>
2014-12-13 17:20:40 +0000

    Add example database output for homepage classes

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,6 @@
 <?php include "./includes/header.php";
 
+//These will be fetched from database
 $targetMoney = 1500000;
 $raisedMoney = 317460;
 
@@ -22,7 +23,55 @@ $raisedMoney = 317460;
 <div class="wrap">
 	<h2>About (short summary)</h2>
 	<h2>Freerunning USP</h2>
-	<h2>Classes</h2>
+</div>
+<div class="block">
+	<div class="wrap">
+		<h2 class="align-center">Gravity gym offers a wide range of lorem ipsums. We also dolor our amets, and feel that consectetur is the best way to adipiscing.</h2>
+		<!--
+			This will be pulled from the database eventually.
+			The closing </a> bracket has been moved to the
+			following line to remove the blank space that
+			appears between inline-block items.
+			http://css-tricks.com/fighting-the-space-between-inline-block-elements/
+		-->
+		<a class="floor-fade" href="#">
+			<div class="rhythmicgymnastics">
+				<h2>Rhythmic</h2>
+			</div>
+		</a
+		><a class="floor-fade w2" href="#">
+			<div class="freerunning">
+				<h2>Freerunning</h2>
+			</div>
+		</a
+		><a class="floor-fade" href="#">
+			<div class="parallelbars">
+				<h2>Parallel bars</h2>
+			</div>
+		</a
+		><a class="floor-fade" href="#">
+			<div class="trampoline">
+				<h2>Trampoline</h2>
+			</div>
+		</a
+		><a class="floor-fade" href="#">
+			<div class="lorem">
+				<h2>Lorem</h2>
+			</div>
+		</a
+		><a class="floor-fade" href="#">
+			<div class="ipsum">
+				<h2>Ipsum</h2>
+			</div>
+		</a
+		><a class="floor-fade" href="#">
+			<div class="dolor">
+				<h2>Dolor</h2>
+			</div>
+		</a>
+	</div>
+</div>
+<div class="wrap">
 	<h2>Testimonials</h2>
 </div>
 <?php include "./includes/footer.php"; ?>

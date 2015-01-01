9ea117867ca0f85a150c78a59dc156efe71bfb13
Dan Hughes <danhughes.dh+github@gmail.com>
2015-01-01 16:52:56 +0000

    Add education copyright exception

--- htdocs/includes/footer.php
+++ htdocs/includes/footer.php
@@ -12,6 +12,7 @@
 			<div class="six columns align-right">
 				<p>&copy; HCI Group One, <?php echo (date("Y") == '2014' ? '2014' : '2014 - '.date("Y")); ?>. All rights reserved.</p>
 				<p class="fontsize-tiny">B. Clements, D. Hughes, R. Streefland and G. Bryant</p>
+				<p class="fontsize-tiny">Images used under education copyright exception</p>
 			</div>
 		</div>
 	</div>

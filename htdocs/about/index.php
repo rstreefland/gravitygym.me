21b4829f01398f309592c7d3f908efebd8b12687
Flujible <george.s.bryant@gmail.com>
2015-01-23 22:26:11 +0000

    Add image and text in a header in About page

--- htdocs/about/index.php
+++ htdocs/about/index.php
@@ -1,5 +1,15 @@
 <?php include "../includes/header.php"; ?>
 
-<h1>TESTES</h1>
+<div class="block pad bg bg2 align-center">
+	<div class="wrap">
+		<div class="row">
+            <h1>Blah Blah</h1>
+		</div>
+	</div>
+</div>
+
+<div>
+    <h2>What is Gravity Gym?</h2>
+</div>
 
 <?php include "../includes/footer.php"; ?>

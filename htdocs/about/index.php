2258be1957f8677a4186446fb7fdcc4c58257bec
Dan Hughes <danhughes.dh+github@gmail.com>
2015-01-24 20:03:58 +0000

    Multiple rows can be contained within a single ".wrap" class

--- htdocs/about/index.php
+++ htdocs/about/index.php
@@ -14,19 +14,13 @@
             <h2>What Is Gravity Gym?</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin, ex nec tempus gravida, orci eros aliquet metus, non vestibulum metus velit sed ante. Proin id placerat tellus. Sed et maximus felis, laoreet malesuada ipsum. Maecenas mollis, dolor id condimentum fermentum, orci dolor gravida justo, imperdiet mollis odio risus ut tortor. Donec dignissim magna in dolor mattis, ut pellentesque nisi dictum. Maecenas a ex sagittis, sollicitudin neque congue, molestie erat. Fusce semper molestie euismod. Phasellus vel viverra felis.</p>
         </div>
-            <div class="five columns">
-                <img src="../images/about/gymnasticClub.jpg">
-            </div> 
-        </div>
-    </div>
-</div>
-
-<div class="wrap">
+		<div class="five columns">
+			<img src="../images/about/gymnasticClub.jpg">
+		</div>
+	</div>
     <div class = "row">
         <h2>Meet the Team</h2>
     </div>
-</div>
-<div class="wrap">
     <div class = "row">
         <div class="three columns">
             <img class="circle image" src="../images/about/dan.jpg">

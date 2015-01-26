1526272f5482e824c759e50a6934510c5488c360
Rhys Streefland <rhysaled.streefland@ntlworld.com>
2015-01-26 18:53:43 +0000

    First implementation of Donate page
    Additional CSS for the page is present in the page-specific section of
    the style.scss file.

--- htdocs/donate/index.php
+++ htdocs/donate/index.php
@@ -1,3 +1,106 @@
 <?php include "../includes/header.php"; ?>
 
+<div class="block pad bg bg1 align-center">
+	<div class="wrap">
+		<div class="row">
+            <h1>Donate</h1>
+		</div>
+	</div>
+</div>
+	<div class="wrap">
+		<div class="row">
+			<div class="donate">
+				<div class="amount">
+					<input type="radio" name="amount" value="10"> £10
+				</div>
+				
+				<div class="description">
+					<div class="row">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="row">
+						<a class="button" href="#">
+							Make donation
+							<br>
+							<i class="fa fa-paypal"></i>	
+							</a>
+					</div>
+				</div>
+			</div>
+			<div class="donate">
+				<div class="amount">
+					<input type="radio" name="amount" value="20"> £20
+				</div>
+				
+				<div class="description">
+					<div class="row">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="row">
+						<a class="button" href="#">
+							Make donation
+							<br>
+							<i class="fa fa-paypal"></i>	
+							</a>
+					</div>
+				</div>
+			</div>
+			<div class="donate">
+				<div class="amount">
+					<input type="radio" name="amount" value="50"> £50
+				</div>
+				
+				<div class="description">
+					<div class="row">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="row">
+						<a class="button" href="#">
+							Make donation
+							<br>
+							<i class="fa fa-paypal"></i>	
+							</a>
+					</div>
+				</div>
+			</div>
+			<div class="donate">
+				<div class="amount">
+					<input type="radio" name="amount" value="100"> £100
+				</div>
+				
+				<div class="description">
+					<div class="row">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="row">
+						<a class="button" href="#">
+							Make donation
+							<br>
+							<i class="fa fa-paypal"></i>	
+							</a>
+					</div>
+				</div>
+			</div>
+			<div class="donate">
+				<div class="amount">
+					<input type="radio" name="amount" value="other"> Other
+				</div>
+				
+				<div class="description">
+					<div class="row">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+					<div class="row">
+						<a class="button" href="#">
+							Make donation
+							<br>
+							<i class="fa fa-paypal"></i>	
+							</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?php include "../includes/footer.php"; ?>

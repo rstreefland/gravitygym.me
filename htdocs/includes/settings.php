8f0796c3e24f44e7c3ae10adeeb456c38fcc9fbd
Dan Hughes <danhughes.dh+github@gmail.com>
2015-03-02 15:31:41 +0000

    Fix stylesheet being fetched before subdirectory is set

--- htdocs/includes/settings.php
+++ htdocs/includes/settings.php
@@ -10,14 +10,13 @@ $filepath = $_SERVER['DOCUMENT_ROOT'];
 
 //Assume production environment, tweak otherwise
 $rootpath='/';
-$stylesheet=$rootpath.'includes/style.php?p=style.scss';
 
 //If running on local machine, use devmode settings (don't cache, use local rather than CDN files)
 //Detect if running in production (gravitygym.me)
 $devMode = ($_SERVER['HTTP_HOST'] != 'gravitygym.me');
 if ($devMode) {
 	$rootpath='/';
-	$stylesheet.='&dev=1';
+	$stylesheetDev='&dev=1';
 }
 
 //Detect if running in staging environment (workshop.xes.io/gravitygym)
@@ -25,7 +24,9 @@ $stagingMode = (strpos($filepath,'workshop') !== false);
 if ($stagingMode) {
 	$devMode = false;
 	$rootpath='/gravitygym/';
+	$stylesheetDev = false;
 }
 
+$stylesheet=$rootpath.'includes/style.php?p=style.scss'.$stylesheetDev;
 require($filepath.$rootpath.'includes/packages/autoload.php');
 ?>

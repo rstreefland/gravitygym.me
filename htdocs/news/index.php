f156dcddd4324d75f6e1ab79737b27b38eda8fc1
Dan Hughes <danhughes.dh+github@gmail.com>
2015-03-03 16:08:58 +0000

    Add news page

--- htdocs/news/index.php
+++ htdocs/news/index.php
@@ -1,28 +1,57 @@
 <?php include "../includes/header.php";
+
+$newsItems = array(
+	//Title => image
+	'fun-run' => array(
+		'image' => $rootpath.'images/uploads/demo.jpg',
+		'title' => 'Fun run',
+		'date' => '3rd February 2014',
+		'content' => $lipsum->getParagraphs(3)
+	),
+	'charity-event' => array(
+		'image' => $rootpath.'images/uploads/charity-event.jpg',
+		'title' => 'Charity event',
+		'date' => '3rd February 2014',
+		'content' => $lipsum->getParagraphs(3)
+	),
+	'personal-trainers-wanted' => array(
+		'image' => $rootpath.'images/uploads/training.jpg',
+		'title' => 'Personal trainers wanted',
+		'date' => '3rd February 2014',
+		'content' => $lipsum->getParagraphs(3)
+	),
+	'win-a-bike' => array(
+		'image' => $rootpath.'images/uploads/bike.jpg',
+		'title' => 'Win a bike',
+		'date' => '3rd February 2014',
+		'content' => $lipsum->getParagraphs(3)
+	)
+);
+
 if (isSet($_GET['p'])) {
+	$settings = $newsItems[$_GET['p']];
+	$settings['URL'] = '//' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
 	$template = $templater->loadModule('newsitem.html');
-	$template->render(array(
-		"image" => $rootpath."images/uploads/demo.jpg",
-		"title" => "Fun run",
-		"date" => "3rd February 2014",
-		"content" => str_replace("\n", "</p><p>", "Content shamelessly stolen from http://www.readingroadrunners.org/mortimer.html
-        
-        The annual Mortimer 10km run will take place on Sunday 28th September 2014 and runs in conjunction with an 8km charity walk and a 3km run which is popular with younger runners. 
+	$template->render($settings);
 
-The course is undulating through the country lanes of Mortimer and starts and finishes on the Mortimer Fairground, RG7 3SW. Thre will be medals for all finishers.  Prizes for the 10km are for the first man and lady and first age category winners. Full details available here 
-
-Around the start/finish area you will find refreshments and stalls.  The Fairground itself has its own play area so it is an ideal setting for a family day out. 
-
-Events start at 10:15am
-
-Entries are open and the closing date for postal entries will be 15th September 2014. Entries on the day will be accepted subject to race limit not being reached. We will be using online entry again this year, please, click on the link below to enter online. (when available).
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis libero at diam volutpat, ac placerat nisi euismod. Suspendisse vestibulum a nisl eget ultrices. Donec interdum quam vel lacinia faucibus. Sed quis arcu varius, elementum neque vitae, dictum urna. Ut et ante gravida, feugiat diam a, laoreet libero. Pellentesque eleifend purus sit amet nisl porta hendrerit. Sed metus est, gravida sed gravida a, sollicitudin a erat."),
-		"URL" => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
-	));
 } else {
 ?>
-<div class="wrap align-center">
-	<a href="./?p=1">News item</a>
+<div class="block pad bg parallax-window align-center" data-parallax="scroll" data-image-src="<?php echo thumb($rootpath.'images/block-bg/news.jpg', 1000);?>">
+	<div class="wrap">
+		<div class="row">
+            <h1>News</h1>
+		</div>
+	</div>
+</div>
+<div class="wrap small">
+	<?php foreach ($newsItems as $slug => $item) { ?>
+	<div class="row pad-top">
+		<h2><a href="./?p=<?php echo $slug;?>"><?php echo $item['title'];?></a></h2>
+		<?php echo explode('</p><p>', $item['content'])[0].'</p>';?>
+		<p class="faint fontsize-tiny"><?php echo $item['date'];?></p>
+	</div>
+	<hr/>
+	<?php } ?>
 </div>
 <?php } include "../includes/footer.php"; ?>

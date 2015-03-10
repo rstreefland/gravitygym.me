29d8ca41d631d931a2022e5e44a543688abc4d14
unknown <george.s.bryant@gmail.com>
2015-03-10 10:58:18 +0000

    Add content to the Events page

--- htdocs/events/index.php
+++ htdocs/events/index.php
@@ -2,14 +2,16 @@
 
 $events = array(
     'an-event' => array(
-        'title' => 'An event',
+        'title' => 'Floor Show',
         'date' => date('jS F Y', time() + 86400),
-        'description' => $lipsum->getSentence(2)
+        'description' => "Gravity Gym is proud to present our first annual floor show, dedicated to showcasing our most talented floor gymnasts! All of these
+            wonderful girls and boys will be showing all who attend the most groundbreaking floor routines that they have created themselves
+            with the help of our friendly and dedicated staff. We look forward to seeing you there!"
     ),
     'another-event' => array(
         'title' => 'Another event',
         'date' => date('jS F Y', time() + 864000),
-        'description' => $lipsum->getSentence(2)
+        'description' => $lipsum->getSentence(5)
     )
 );
 
@@ -36,7 +38,8 @@ $events = array(
             <h2>Past events</h2>
             <div>
                 <h3>2nd January 2015 - Fun run</h3>
-                <?php echo $lipsum->getSentence(3);?>
+                <p>The fun run was a big success, with over 100 people taking part, we managed to raise almost £500 for the club! Thank you very much
+                    to all those who participated and all those who helped organise the run.</p>
             </div>
         </div>
     </div>
